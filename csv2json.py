cd84bfe6227812c0526c6b87513531b4bd4dbb7e
Dito Manavalan <dito.john@gmail.com>
2022-08-17 12:08:34 -0500

    Removed obsolete scripts

--- csv2json.py
+++
@@ -1,6 +0,0 @@
-from pyspark.sql import SparkSession
-
-spark = SparkSession.builder.appName('helloWorld').getOrCreate()
-srcdf = spark.read.csv('training.csv', header=True, inferSchema=True)
-srcdf.write.format("json").text()
-
