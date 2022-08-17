c7aa6c06e99f6bac55845aaff5df46c8507fd6c2
Dito Manavalan <dito.john@gmail.com>
2022-08-17 12:13:45 -0500

    Added pyspark scripts for word count

--- local-pyspark-word-count.py
+++ local-pyspark-word-count.py
@@ -1,4 +1,11 @@
+import os
+import sys
 from pyspark import SparkContext, SparkConf
+
+os.environ['PYSPARK_PYTHON'] = sys.executable
+os.environ['PYSPARK_DRIVER_PYTHON'] = sys.executable
+os.environ['HADOOP_HOME'] = "C:\JMCube\Make\hadoop"
+os.environ['hadoop.home.dir'] = "C:\JMCube\Make\hadoop"
 
 conf = SparkConf().setAppName('MyFirstStandaloneApp')
 sc = SparkContext(conf=conf)
